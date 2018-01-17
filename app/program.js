83af258c0f532f5316ed4da644124324117561c7
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-01-17 15:04:55 +0100

    do task

---
+++ app/program.js
@@ -0,0 +1,29 @@
+var OSinfo = require('../modules/OSinfo');
+
+process.stdin.setEncoding('utf-8');
+console.log('Dostępne komendy to /exit | /info | /getOSinfo');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if(input !== null) {
+    	var instruction = input.toString().trim();
+    	switch (instruction) {
+    		case '/exit':
+    			process.stdout.write('Quitting app!\n');
+    			process.exit();
+    		break;
+    		
+    		case '/info':
+    			console.log(process.versions);
+    		break;
+
+    		case '/getOSinfo':
+    			OSinfo.print();
+    		break;
+
+    		default:
+    			process.stderr.write('Wrong instruction\n');
+    	};
+    }  
+});
+
+
