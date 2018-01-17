a8c5c72195bc1bdff1abd8acc16481b9591ef64a
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-01-17 18:18:48 +0100

    do task

---
+++ index.js
@@ -0,0 +1,29 @@
+var osinfo = require('./modules/OSinfo');
+
+process.stdin.setEncoding('utf-8');
+console.log('Dostępne komendy to /exit | /info | /getOSinfo');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if(input !== null) {
+    	var instruction = input.toString().trim();
+    	switch (instruction) {
+    		case '/exit':
+    			process.stdout.write('Quitting app!\n');
+    			process.exit();
+    		break;
+    		
+    		case '/info':
+    			console.log(process.versions);
+    		break;
+
+    		case '/getOSinfo':
+    			osinfo.print();
+    		break;
+
+    		default:
+    			process.stderr.write('Wrong instruction\n');
+    	};
+    }  
+});
+
+
--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,6 @@
 var os = require('os');
 var time = require('../modules/Time');
+var colors = require('colors'); 
 
 function getOSinfo() {
 	var type = os.type();
@@ -14,12 +15,12 @@ function getOSinfo() {
 		type = 'Windows';
 	}
 		
-	console.log('System:', type);
-	console.log('Release:', release);
-	console.log('CPU model:', cpu);
-	console.log('Uptime: ~', time.workingTime(uptime));
-	console.log('User name:', userInfo.username);
-	console.log('Home dir:', userInfo.homedir);
+	console.log(colors.grey('System:'), type);
+	console.log(colors.red('Release:'), release);
+	console.log(colors.magenta('CPU model:'), cpu);
+	console.log(colors.green('Uptime: ~'), time.workingTime(uptime));
+	console.log(colors.yellow('User name:'), userInfo.username);
+	console.log(colors.rainbow('Home dir:'), userInfo.homedir);
 }
 
 exports.print = getOSinfo;
